ed5788564abeb2828f8d3ccbb9cf62cb324a42a4
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-18 22:41:33 +0000

    created func to print int

---
+++ my_int.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_int - prints integers
+ * @a: int to be printed
+ * Return: characters printed
+ */
+int print_int(int a)
+{
+	int len = 0;
+
+	if (a == -2147483648)
+		return (print_string("-2147483648"));
+	if (a == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	if (a < 0)
+	{
+		_putchar('-');
+		a = -a;
+		len++;
+	}
+	if ((a / 10) != 0)
+	{
+		len += print_int(a / 10);
+	}
+	_putchar((a % 10) + '0');
+	len++;
+	return (len);
+}
